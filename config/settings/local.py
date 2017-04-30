57f5e26efc610e8d6f96c1ea7ed18866e8ad3eb9
Mingdong Tan <mingdongt@student.unimelb.edu.au>
2017-04-30 21:55:09 +1000

    bugfix/django-cookiecutter secret key.

--- config/settings/local.py
+++ config/settings/local.py
@@ -20,7 +20,7 @@ TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 # ------------------------------------------------------------------------------
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 # Note: This key only used for development and testing.
-SECRET_KEY = env('DJANGO_SECRET_KEY', default='$Xk&5;d7*nVq(!EY|e*iRrL6G2%B=x;$qQ3Rl1@SjHCXe7]MDg')
+SECRET_KEY = env('DJANGO_SECRET_KEY', default='Xk&5;d7*nVq(!EY|e*iRrL6G2%B=x;$qQ3Rl1@SjHCXe7]MDg')
 
 # Mail settings
 # ------------------------------------------------------------------------------
