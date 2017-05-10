a1c1c9a46b756fc05bb5d61269b1ad0de8c9d441
Mingdong Tan <mingdongt@student.unimelb.edu.au>
2017-05-10 18:33:32 +1000

    Urls for main pages.

--- getdwelling/dwelling/urls.py
+++ getdwelling/dwelling/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^source', views.DataSourceView, name='data_source'),
+    url(r'^analyze$', views.DataAnalyzeView().as_view(), name='data_analyze'),
 ]
