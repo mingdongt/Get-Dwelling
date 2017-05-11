8bd3880a48ad63ab65ac810563764b2969cc5c58
Mingdong Tan <mingdongt@student.unimelb.edu.au>
2017-05-11 20:26:59 +1000

    Add temporary views.

--- getdwelling/dwelling/views.py
+++ getdwelling/dwelling/views.py
@@ -1,18 +1,19 @@
 
 from django.views.generic import TemplateView
 
+#FIXME
+from django.http import HttpResponse
+
 
 class IndexView(TemplateView):
     """ Index View for the website. """
 
     template_name = 'dwelling/index.html'
 
-#FIXME
-from django.http import HttpResponse
-
 
 def DataSourceView(request):
     return HttpResponse("data source")
+
 
 def DataAnalyzeView(request):
     return HttpResponse("data analyze")
